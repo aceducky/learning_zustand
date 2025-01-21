6850d32c970495ad6b974dc74bf6c5128a0fe79c
SrinivasD <115702816+aceducky@users.noreply.github.com>
2025-01-21 21:03:52 +0530

    add devtools also working functionality wise same as the previous v2 version as of now

--- src/components/q2/useColorStore.js
+++ src/components/q2/useColorStore.js
@@ -1,17 +1,22 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
-export const useColorStore = create((set) => ({
-    name: "",
-    className: "",
-    setName: (_name) => set({ name: _name }),
-    setClassName: (_className) => set({ className: _className }),
-    /*
-       setName: (name) => set({ name: name })
-       setClassName: (className) => set({ className: className })
-       
-       Or
-       
-       setName: (name) => set({ name})
-        setClassName: (className) => set({ className }) 
-       */
-}));
+export const useColorStore = create(
+    devtools(
+        (set) => ({
+            name: "",
+            className: "",
+            setName: (_name) => set({ name: _name }),
+            setClassName: (_className) => set({ className: _className }),
+            /*
+                           setName: (name) => set({ name: name })
+                           setClassName: (className) => set({ className: className })
+                           
+                           Or
+                           
+                           setName: (name) => set({ name})
+                            setClassName: (className) => set({ className }) 
+                           */
+        })
+    )
+);
